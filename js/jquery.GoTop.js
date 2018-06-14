faf460aba76cc6b5a2063c8e4eff8f40c92db8d2
ruohua <54871181@qq.com>
2018-06-14 16:25:02 +0800

    添加share feedback

---
+++ js/jquery.GoTop.js
@@ -0,0 +1,27 @@
+//回到顶部：滚动定位
+var $btnTop = $('.feedback .top');
+var $btnCode = $('.feedback .code');
+var $qrCode = $('.feedback .code-layer');
+function GoTop(){
+	var scTop = $(document).scrollTop();
+	if(scTop >= 500){
+		$btnTop.fadeIn(500);
+	} else{
+		$btnTop.fadeOut(500);
+	};
+};
+GoTop();
+$(window).scroll(function(){
+	GoTop();
+});
+//回到顶部
+$btnTop.click(function(e){
+	e.preventDefault();
+	$('html,body').animate({scrollTop:0},500);
+});
+//二维码
+$btnCode.hover(function(){
+	$qrCode.show();
+}, function(){
+	$qrCode.hide();
+});
