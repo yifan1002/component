aed5bf0992707731fc3aa2d4edb1ab301269a044
ruohua <54871181@qq.com>
2019-05-20 17:40:52 +0800

    调整tabs插件，可链接跳转

--- assets/js/jquery-tabs.js
+++ assets/js/jquery-tabs.js
@@ -7,7 +7,8 @@
 	//默认参数
 	var defaults = {
 		evt: 1,	//事件：0-鼠标滑过切换，其他全部为点击
-		ant: 1	//动画：1-直接显示隐藏；2-淡入淡出；3-滑上滑下
+		ant: 1, //动画：1-直接显示隐藏；2-淡入淡出；3-滑上滑下
+		jump: 0 //链接调整：0-不是，阻止默认链接，其他全部为默认链接
 	};
 	
 	//定义构造函数
@@ -25,12 +26,15 @@
 			var _ = this,
 				_el = $(this.element),
 				evt = 'click',
-				ant = this.options.ant;
+				ant = this.options.ant,
+				jump = this.options.jump;
 			if(this.options.evt === 0) {
 				evt = 'mouseover';
 			};
 			_el.children('.tab').on(evt, function(e) {
-				e.preventDefault();
+				if (jump === 0) {
+					e.preventDefault();
+				}
 				if (!$(this).hasClass('disabled')) {
 					var g = $(this).parent('.tabs').attr('tabs-group');
 					var href = $(this).children('a').attr('href');
