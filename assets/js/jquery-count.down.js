a1f1111f33f3ff6eb7093c30aab2ddd12ac001b3
ruohua <54871181@qq.com>
2019-10-30 19:13:06 +0800

    增加倒计时插件

--- assets/js/jquery-count.down.js
+++ assets/js/jquery-count.down.js
@@ -1,15 +1,17 @@
 //$('#countDown').formCount({
-//	data: '2019/10/1 00:00:00'
+//	endTime: '2019/10/1 00:00:00',
+//	curTime: '2019/10/1 00:00:00'
 //});
 //倒计时
 ;
-(function($, window, document, undefined){
+(function($, window, document, undefined) {
 	//默认参数
 	var defaults = {
 		endTime: '2099/10/1 00:00:00', //当前时间
-		curTime: new Date() //结束时间，默认本地当前时间
+		curTime: new Date(), //结束时间，默认本地当前时间
+		timeoffset: -480 //东8区默认时区和格林威治时间差，不需要外部传参
 	};
-	
+
 	//定义构造函数
 	function CountDown(element, setting) {
 		this.element = element;
@@ -25,21 +27,27 @@
 			var _ = this,
 				_el = $(this.element),
 				endTime = this.options.endTime,
-				curTime = new Date(this.options.curTime).getTime();
-			
+				curTime = new Date(this.options.curTime).getTime(),
+				timeoffset = this.options.timeoffset;
+			//console.log('原本时间差：' + timeoffset)
+
 			_countDown(curTime);
-			window.setInterval(function(){
+			window.setInterval(function() {
 				curTime += 1000;
 				_countDown(curTime);
 			}, 1000);
-			
+
 			function _countDown(time) {
 				var startTime = new Date(time), //当前时间
 					stopTime = new Date(endTime), //结束时间
-					diffTime= stopTime.getTime() - startTime.getTime();
-				if (diffTime > 0) {
-					var diffMs = stopTime.getTime() - startTime.getTime(), //倒计时毫秒数
-						diffD = parseInt(diffMs / (60 * 60 * 24 * 1000)), //转换为天
+					diffMs = stopTime.getTime() - startTime.getTime(), //校正前的倒计时毫秒数
+					timeoffsetNew = new Date().getTimezoneOffset(), //新时区和格林威治时间差
+					timeoffsetNeed = timeoffset - timeoffsetNew, //需要矫正的时间差
+					diffMs = diffMs + timeoffsetNeed * 60 * 1000; //校正后的倒计时毫秒数
+				//console.log('新时间差：' + timeoffsetNew);
+				//console.log('需要矫正的时间差：' + timeoffsetNeed);
+				if(diffMs > 0) {
+					var diffD = parseInt(diffMs / (60 * 60 * 24 * 1000)), //转换为天
 						D = parseInt(diffMs) - parseInt(diffD * 60 * 60 * 24 * 1000), //除去天的毫秒数
 						diffH = parseInt(D / (60 * 60 * 1000)), //除去天的毫秒数转换成小时
 						H = D - diffH * 60 * 60 * 1000, //除去天、小时的毫秒数
@@ -47,19 +55,19 @@
 						M = H - diffM * 60 * 1000, //除去天、小时、分的毫秒数
 						diffS = M / 1000; //除去天、小时、分的毫秒数转化为秒
 					_el.html('<span>' + diffD + "</span>天<span> " + diffH + "</span> 小时 <span>" + diffM + "</span> 分 <span>" + diffS + "</span> 秒");
-				} else{
+				} else {
 					_el.html('已结束！');
 				}
 			}
-			
+
 		}
 	};
-	
+
 	//定义插件
 	$.fn.countDown = function(options) {
-        return this.each(function(index, element) {
-            element.countDown = new CountDown(element, options);
-        });
+		return this.each(function(index, element) {
+			element.countDown = new CountDown(element, options);
+		});
 	};
-	
+
 })(jQuery, window, document);
